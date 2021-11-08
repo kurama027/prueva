41a6ef21103354d34f1c00d500ae977021cba41c
kurama027 <robertoflores@upeu.edu.pe>
2021-11-08 18:06:46 -0500

    implementandosrc

---
+++ src/main/java/com/backed/Lost/FloresR1Application.java
@@ -0,0 +1,13 @@
+package com.backed.Lost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloresR1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloresR1Application.class, args);
+	}
+
+}
